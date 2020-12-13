f42ac6819a0e960d73264597b3161adb2822140d
paul <paulnjoroge789016@gmail.com>
2020-12-13 16:35:19 +0300

    retrofit di configuration

--- Domain/src/main/java/com/portfolio/domain/DataSource.kt
+++ Domain/src/main/java/com/portfolio/domain/DataSource.kt
@@ -7,8 +7,8 @@ package com.portfolio.domain
  * A good programmer writes code that can be understood
  * by humans
  */
-interface DataSource {
 
+interface DataSource {
     fun getData() : String
 
 }
