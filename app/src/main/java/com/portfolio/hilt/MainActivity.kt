80f3057a7ddf2606d72ee911b35aa1ae3879523a
paul <paulnjoroge789016@gmail.com>
2021-02-05 20:16:19 +0300

    handling error message

--- app/src/main/java/com/portfolio/hilt/MainActivity.kt
+++ app/src/main/java/com/portfolio/hilt/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.portfolio.hilt
 
+import android.graphics.Point
 import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
 import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.github.amlcurran.showcaseview.ShowcaseView
+import com.github.amlcurran.showcaseview.targets.ActionViewTarget
 import com.portfolio.hilt.adapters.PostPagedListAdapter
 import com.portfolio.hilt.databinding.ActivityMainBinding
 import com.portfolio.hilt.viewmodels.PostViewModel
@@ -44,5 +50,25 @@ class MainActivity : AppCompatActivity() {
         })
 
 
+        postViewModel.errorMessage.observe(this, { errorMessage ->
+            createErrorPopupDialog(errorMessage)
+        })
+
+
+
+    }
+
+    private fun createErrorPopupDialog(errorMessage: String?) {
+
+        val alertDialogBuilder =  AlertDialog.Builder(this@MainActivity)
+        alertDialogBuilder.apply {
+
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuInflater = MenuInflater(this@MainActivity)
+        menuInflater.inflate(R.menu.testmenu, menu)
+        return true
     }
 }
